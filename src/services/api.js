3def04d9b27d244e79e94e541b827ae6c755521f
Davi Silva <davisilvaphoto@gmail.com>
2022-02-04 15:45:18 -0300

    refactor files in services folder

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,10 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export const key = 'a0ee0dfccca7228da50a76644d16f0d35febc0c5'
+export const key = 'a0ee0dfccca7228da50a76644d16f0d35febc0c5';
 
-const api = axios.create({
-    baseURL: 'https://api-ssl.bitly.com/v4',
-    headers: {
-        'Authorization': `Bearer ${key}`
-    }
-})
-
-export default api
-
+export const api = axios.create({
+  baseURL: 'https://api-ssl.bitly.com/v4',
+  headers: {
+    Authorization: `Bearer ${key}`,
+  },
+});
