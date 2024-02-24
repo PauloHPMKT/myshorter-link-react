992af6f0cef2b4ae6767e95f072ce8b47e300e9a
Paulo Sergio <pauloserg861@gmail.com>
2024-02-24 15:10:59 -0300

    chore: add eslint e prettier

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 export const key = process.env.REACT_APP_BITLY_KEY;
+const baseURL = process.env.REACT_APP_BITLY_URL;
 
 export const api = axios.create({
-  baseURL: 'https://api-ssl.bitly.com/v4',
+  baseURL,
   headers: {
     Authorization: `Bearer ${key}`,
   },
